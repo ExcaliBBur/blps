91ae798f793165b93a809c6655da55358b5bfcfc
ShipIM <ilya.shipunov@gmail.com>
2024-03-18 19:00:07 +0300

    roles hierarchy implementation

--- lab/src/main/java/com/example/lab/model/enumeration/RoleEnum.java
+++ lab/src/main/java/com/example/lab/model/enumeration/RoleEnum.java
@@ -1,7 +1,5 @@
 package com.example.lab.model.enumeration;
 
-import org.springframework.security.core.GrantedAuthority;
-
 public enum RoleEnum {
 
     ROLE_ADMIN,
--- lab/src/main/java/com/example/lab/repository/PrivilegeRepository.java
+++ lab/src/main/java/com/example/lab/repository/PrivilegeRepository.java
@@ -7,9 +7,19 @@ import reactor.core.publisher.Flux;
 
 public interface PrivilegeRepository extends ReactiveCrudRepository<Privilege, Long> {
 
-    @Query("select * from privilege p " +
-            "join role_privilege rp on p.id = rp.privilege_id " +
-            "where rp.role_id = :role")
+    @Query("with recursive role_hierarchy as ( " +
+            "select id, name " +
+            "from role " +
+            "where id = :id " +
+            "union all " +
+            "select rr.second_role_id as id, r.name " +
+            "from role_hierarchy rh " +
+            "join role_role rr on rh.id = rr.first_role_id " +
+            "join role r on rr.second_role_id = r.id) " +
+            "select p.id, p.name " +
+            "from role_hierarchy rh " +
+            "join role_privilege rp on rh.id = rp.role_id " +
+            "join privilege p on rp.privilege_id = p.id")
     Flux<Privilege> findPrivilegesByRoleId(Long id);
 
 }
