849ddd9a0bfb2f703240c16ec5e51acb05df1918
Alexander <excalibburn@shinra-tensei.ru>
2024-04-13 14:40:22 +0300

    declarative transactions isolation level

--- lab/src/main/java/com/example/lab/service/AdminPanelService.java
+++ lab/src/main/java/com/example/lab/service/AdminPanelService.java
@@ -7,59 +7,33 @@ import com.example.lab.model.enumeration.RoleEnum;
 import com.example.lab.repository.ReservationRepository;
 import com.example.lab.repository.TicketRepository;
 import com.example.lab.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class AdminPanelService {
     private final TicketRepository ticketRepository;
     private final ReservationRepository reservationRepository;
-//    private final PlatformTransactionManager transactionManager;
     private final UserRepository userRepository;
 
-    @Transactional
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public Mono<Reservation> createTicketAndReservation(Ticket ticket, Reservation reservation) {
-//        TransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
-//        TransactionStatus transaction = transactionManager.getTransaction(transactionDefinition);
 
         return ticketRepository.save(ticket)
                 .flatMap(t -> {
                     reservation.setTicket(t.getId());
                     return reservationRepository.save(reservation);
-//                            .flatMap(r -> {
-//                                if (Objects.isNull(r)) {
-//                                    transactionManager.rollback(transaction);
-//                                } else {
-//                                    transactionManager.commit(transaction);
-//                                }
-//                                return Mono.just(r);
-//                            });
                 });
     }
 
-    @Transactional
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public Mono<User> createUserAndSetModeratorRole(User user) {
-//        TransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
-//        TransactionStatus transaction = transactionManager.getTransaction(transactionDefinition);
 
         return userRepository.save(user)
                 .flatMap(u -> userRepository.updateUserRole(u.getId(), String.valueOf(RoleEnum.ROLE_MODERATOR)));
-//                .flatMap(u -> {
-//                    if (Objects.isNull(u)) {
-//                        transactionManager.rollback(transaction);
-//                    } else {
-//                        transactionManager.commit(transaction);
-//                    }
-//                    return Mono.just(u);
-//                });
     }
 }
